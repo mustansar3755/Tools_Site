39ebf3262edd928558a5d8487d356abb73de5811
mustansar3755 <mustansar3755hussain@gmail.com>
2025-10-29 13:52:53 +0500

    Data.js File Update Some Other Tools Name & Description

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,12 +1,21 @@
-import compress from '../assets/images/compressed.svg'
-import split from '../assets/images/split.svg'
-import merge from '../assets/images/merge.svg'
-import convert from '../assets/images/two-arrows.svg'
-import resize from '../assets/images/resize.svg'
-import imgBG from '../assets/images/imgBG_remo.svg'
+import compress from "../assets/images/compressed.svg";
+import split from "../assets/images/split.svg";
+import merge from "../assets/images/merge.svg";
+import convert from "../assets/images/two-arrows.svg";
+import resize from "../assets/images/resize.svg";
+import imgBG from "../assets/images/imgBG_remo.svg";
+import editImg from "../assets/images/edit-image.svg";
+import cropImg from "../assets/images/image-editing.svg";
+import BlurImg from "../assets/images/shape.svg";
 
 export default {
-    compress,split,merge,convert,
-    resize,
-    imgBG
-}
+  compress,
+  split,
+  merge,
+  convert,
+  cropImg,
+  editImg,
+  resize,
+  imgBG,
+  BlurImg
+};
